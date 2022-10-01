9689268fb6cadcc9878848a979e9316676d03e37
jgrss <jbgraesser@gmail.com>
2022-10-01 16:16:29 -0600

    add load_model() method to base import

--- src/cultionet/__init__.py
+++ src/cultionet/__init__.py
@@ -1,5 +1,5 @@
 __path__: str = __import__('pkgutil').extend_path(__path__, __name__)
 __version__ = '1.1.1'
-from .model import fit, predict
+from .model import fit, load_model, predict
 
-__all__ = ['fit', 'predict']
+__all__ = ['fit', 'load_model', 'predict']
