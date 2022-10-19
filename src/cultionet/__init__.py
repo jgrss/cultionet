51c6a0436219929b38cd1663035fe833a4bea7b8
Jordan Graesser <jgrss@users.noreply.github.com>
2022-10-19 12:10:48 -0600

    v1.3.3

--- src/cultionet/__init__.py
+++ src/cultionet/__init__.py
@@ -1,5 +1,5 @@
 __path__: str = __import__('pkgutil').extend_path(__path__, __name__)
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 from .model import fit, load_model, predict
 
 __all__ = ['fit', 'load_model', 'predict']
