a4a04b5f72048ef423e98172fcf0f7cb2560617a
jgrss <jbgraesser@gmail.com>
2022-10-20 14:30:31 -0600

    added process

--- src/cultionet/__init__.py
+++ src/cultionet/__init__.py
@@ -1,5 +1,5 @@
 __path__: str = __import__('pkgutil').extend_path(__path__, __name__)
 __version__ = '1.3.3'
-from .model import fit, load_model, predict
+from .model import fit, fit_maskrcnn, load_model, predict
 
-__all__ = ['fit', 'load_model', 'predict']
+__all__ = ['fit', 'fit_maskrcnn', 'load_model', 'predict']
