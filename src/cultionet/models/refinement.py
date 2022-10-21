17a644fd4d375a35c64568f528adcc1b0c121335
jgrss <jbgraesser@gmail.com>
2022-10-21 15:49:06 -0600

    change layers

--- src/cultionet/models/refinement.py
+++ src/cultionet/models/refinement.py
@@ -1,6 +1,7 @@
 from . import model_utils
 
 import torch
+from torch_geometric import nn
 
 
 class RefineConv(torch.nn.Module):
@@ -18,26 +19,36 @@ class RefineConv(torch.nn.Module):
         self.gc = model_utils.GraphToConv()
         self.cg = model_utils.ConvToGraph()
 
-        self.seq = torch.nn.Sequential(
-            torch.nn.Conv2d(
-                in_channels, mid_channels, kernel_size=1, padding=0, bias=False, padding_mode='replicate'
-            ),
-            torch.nn.BatchNorm2d(mid_channels),
-            torch.nn.ELU(alpha=0.1, inplace=False),
-            torch.nn.Conv2d(
-                mid_channels, mid_channels, kernel_size=3, padding=1, bias=False, padding_mode='replicate'
-            ),
-            torch.nn.BatchNorm2d(mid_channels),
-            torch.nn.Dropout(dropout),
-            torch.nn.Conv2d(
-                mid_channels, mid_channels, kernel_size=3, padding=1, bias=False, padding_mode='replicate'
-            ),
-            torch.nn.BatchNorm2d(mid_channels),
-            torch.nn.Dropout(dropout),
-            torch.nn.Conv2d(
-                mid_channels, out_channels, kernel_size=1, padding=0, bias=False, padding_mode='replicate'
-            )
+        conv2d_1 = torch.nn.Conv2d(
+            in_channels, mid_channels, kernel_size=1, padding=0, bias=False, padding_mode='replicate'
         )
+        conv2d_2 = torch.nn.Conv2d(
+            mid_channels, mid_channels, kernel_size=3, padding=1, bias=False, padding_mode='replicate'
+        )
+        conv2d_3 = torch.nn.Conv2d(
+            mid_channels, out_channels, kernel_size=1, padding=0, bias=False, padding_mode='replicate'
+        )
+
+        self.seq = nn.Sequential(
+            'x',
+            [
+                (conv2d_1, 'x -> h'),
+                (torch.nn.BatchNorm2d(mid_channels), 'h -> h'),
+                (torch.nn.ELU(alpha=0.1, inplace=False), 'h -> h'),
+                (conv2d_2, 'h -> h'),
+                (torch.nn.BatchNorm2d(mid_channels), 'h -> h'),
+                (torch.nn.ELU(alpha=0.1, inplace=False), 'h -> h'),
+                (torch.nn.Dropout(dropout), 'h -> h'),
+                (conv2d_2, 'h -> h'),
+                (torch.nn.BatchNorm2d(mid_channels), 'h -> h'),
+                (torch.nn.ReLU(inplace=False), 'h -> h'),
+                (torch.nn.Dropout(dropout), 'h -> h'),
+                (conv2d_3, 'h -> h')
+            ]
+        )
+
+    def add_res(self, h1: torch.Tensor, h2: torch.Tensor) -> torch.Tensor:
+        return h1 + h2
 
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
@@ -55,7 +66,6 @@ class RefineConv(torch.nn.Module):
             height,
             width
         )
-
         x = self.seq(x)
 
         return self.cg(x)
