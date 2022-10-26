3124f540012cbdd7577491e8fe8197a23dcfaee2
jgrss <jbgraesser@gmail.com>
2022-10-26 07:25:18 -0600

    updated losses

--- src/cultionet/losses/losses.py
+++ src/cultionet/losses/losses.py
@@ -3,91 +3,29 @@ import typing as T
 import attr
 import torch
 import torch.nn.functional as F
-from torchmetrics.functional import stat_scores
 
 
-@attr.s
-class F1Score(object):
-    """F1-score
-
-    Reference:
-        https://gist.github.com/SuperShinyEyes/dcc68a08ff8b615442e3bc6a9b55a354
-    """
-    num_classes: int = attr.ib(validator=attr.validators.instance_of(int))
-    epsilon: T.Optional[float] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(float)),
-                                         default=1e-7)
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
-        """
-        Performs a single forward pass
-
-        Args:
-            inputs: Predictions from model (probabilities, logits or labels).
-            targets: Ground truth values.
-
-        Returns:
-            f1-score (float)
-        """
-        inputs = inputs.contiguous().view(-1)
-        targets = targets.contiguous().view(-1)
-
-        tp, fp, tn, fn, sup = stat_scores(inputs, targets, num_classes=self.num_classes)
-
-        precision = tp / (tp + fp + self.epsilon)
-        recall = tp / (tp + fn + self.epsilon)
-
-        f1 = 2.0 * (precision*recall) / (precision + recall + self.epsilon)
-        f1 = f1.clamp(min=self.epsilon, max=1.0-self.epsilon)
-
-        return f1.mean()
-
-
-@attr.s
-class MatthewsCorrcoef(object):
-    """Matthews Correlation Coefficient
-
-    Source:
-        https://en.wikipedia.org/wiki/Matthews_correlation_coefficient
-    """
-    num_classes: int = attr.ib(validator=attr.validators.instance_of(int))
-    inputs_are_logits: T.Optional[bool] = attr.ib(default=True)
-
-    def __attrs_post_init__(self):
-        super(MatthewsCorrcoef, self).__init__()
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        """
-        Performs a single forward pass
-
-        Args:
-            inputs: Predictions from model (probabilities, logits or labels).
-            targets: Ground truth values.
-
-        Returns:
-            Matthews correlation coefficient (float)
-        """
+class ClassifierPreprocessing(object):
+    def preprocess(
+        self,
+        inputs: torch.Tensor,
+        targets: torch.Tensor
+    ) -> T.Tuple[torch.Tensor, torch.Tensor]:
         if self.inputs_are_logits:
-            inputs = F.softmax(inputs, dim=1).argmax(dim=1)
+            if self.apply_transform:
+                inputs = F.softmax(inputs, dim=1, dtype=inputs.dtype)
+            targets = F.one_hot(
+                targets.contiguous().view(-1), inputs.shape[1]
+            ).float()
+        else:
+            inputs = inputs.unsqueeze(1)
+            targets = targets.unsqueeze(1)
 
-        tp, fp, tn, fn, sup = stat_scores(inputs, targets, num_classes=self.num_classes)
-
-        numerator = (tp * tn) - (fp * fn)
-        denominator = torch.sqrt(((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)))
-
-        matthews_corr_coef = numerator / denominator
-        matthews_corr_coef = torch.nan_to_num(matthews_corr_coef, nan=0.0, neginf=0.0, posinf=0.0)
-
-        return matthews_corr_coef
+        return inputs, targets
 
 
 @attr.s
-class TanimotoDistanceLoss(object):
+class TanimotoDistanceLoss(ClassifierPreprocessing):
     """Tanimoto distance loss
 
     Reference:
@@ -132,28 +70,63 @@ class TanimotoDistanceLoss(object):
         Returns:
             Tanimoto distance loss (float)
         """
-        if self.inputs_are_logits:
-            if self.apply_transform:
-                inputs = F.softmax(inputs, dim=1)
-            targets = F.one_hot(targets.contiguous().view(-1), inputs.shape[1]).float()
-        else:
-            inputs = inputs.unsqueeze(1)
-            targets = targets.unsqueeze(1)
+        inputs, targets = self.preprocess(inputs, targets)
 
         weights = torch.reciprocal(torch.square(self.volume))
         new_weights = torch.where(torch.isinf(weights), torch.zeros_like(weights), weights)
-        weights = torch.where(torch.isinf(weights), torch.ones_like(weights) * new_weights.max(), weights)
+        weights = torch.where(
+            torch.isinf(weights), torch.ones_like(weights) * new_weights.max(), weights
+        )
         intersection = (targets * inputs).sum(dim=0)
         sum_ = (targets * targets + inputs * inputs).sum(dim=0)
         num_ = (intersection * weights) + self.smooth
         den_ = ((sum_ - intersection) * weights) + self.smooth
         tanimoto = num_ / den_
-        loss = (1.0 - tanimoto)
+        loss = 1.0 - tanimoto
 
         if self.class_weights is not None:
             loss = loss * self.class_weights
 
         return loss.sum()
+
+
+@attr.s
+class CrossEntropyLoss(ClassifierPreprocessing):
+    """Huber loss
+    """
+    class_weights: T.Optional[torch.Tensor] = attr.ib(
+        default=None,
+        validator=attr.validators.optional(validator=attr.validators.instance_of(torch.Tensor))
+    )
+    inputs_are_logits: T.Optional[bool] = attr.ib(
+        default=True,
+        validator=attr.validators.optional(validator=attr.validators.instance_of(bool))
+    )
+    apply_transform: T.Optional[bool] = attr.ib(
+        default=True,
+        validator=attr.validators.optional(validator=attr.validators.instance_of(bool))
+    )
+
+    def __attrs_post_init__(self):
+        super(CrossEntropyLoss, self).__init__()
+        self.loss_func = torch.nn.CrossEntropyLoss(weight=self.class_weights)
+
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        """Performs a single forward pass
+
+        Args:
+            inputs: Predictions from model.
+            targets: Ground truth values.
+
+        Returns:
+            Loss (float)
+        """
+        inputs, targets = self.preprocess(inputs, targets)
+
+        return self.loss_func(inputs, targets)
 
 
 @attr.s
@@ -192,30 +165,6 @@ class QuantileLoss(object):
         loss = torch.cat(losses, dim=1).sum(dim=1).mean()
 
         return loss
-
-
-@attr.s
-class MSELoss(object):
-    """Mean squared error loss
-    """
-    def __attrs_post_init__(self):
-        super(MSELoss, self).__init__()
-        self.loss_func = torch.nn.MSELoss()
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        """Performs a single forward pass
-
-        Args:
-            inputs: Predictions from model.
-            targets: Ground truth values.
-
-        Returns:
-            Loss (float)
-        """
-        return self.loss_func(inputs, targets)
 
 
 @attr.s
