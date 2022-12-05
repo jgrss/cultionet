d4334d3d11040ca6b5ebc6c503275d12e63dd98e
jgrss <jbgraesser@gmail.com>
2022-12-05 08:23:08 -0800

    remove convstar norm and activation

--- src/cultionet/models/convstar.py
+++ src/cultionet/models/convstar.py
@@ -147,32 +147,20 @@ class StarRNN(torch.nn.Module):
         )
 
         padding = int(kernel_size / 2)
-        self.final = torch.nn.Sequential(
-            torch.nn.Conv2d(
-                hidden_dim,
-                hidden_dim,
-                kernel_size,
-                padding=padding
-            ),
-            torch.nn.BatchNorm2d(hidden_dim),
-            torch.nn.LeakyReLU(inplace=False)
+        self.final = torch.nn.Conv2d(
+            hidden_dim,
+            hidden_dim,
+            kernel_size,
+            padding=padding
         )
         # Crop-type layer
         if self.crop_type_layer:
-            self.final_last = torch.nn.Sequential(
-                torch.nn.Conv2d(
-                    hidden_dim, num_classes_last, kernel_size, padding=padding
-                ),
-                torch.nn.BatchNorm2d(num_classes_last),
-                torch.nn.LeakyReLU(inplace=False)
+            self.final_last = torch.nn.Conv2d(
+                hidden_dim, num_classes_last, kernel_size, padding=padding
             )
         else:
-            self.final_last = torch.nn.Sequential(
-                torch.nn.Conv2d(
-                    hidden_dim, 2, kernel_size, padding=padding
-                ),
-                torch.nn.BatchNorm2d(2),
-                torch.nn.LeakyReLU(inplace=False)
+            self.final_last = torch.nn.Conv2d(
+                hidden_dim, 2, kernel_size, padding=padding
             )
 
     def __call__(self, *args, **kwargs):
