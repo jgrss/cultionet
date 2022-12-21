33fd6d0c78141d475867bbc3bf53bbddda2b22e3
jgrss <jbgraesser@gmail.com>
2022-12-21 02:07:44 -0600

    sigmoid edge output

--- src/cultionet/models/cultio.py
+++ src/cultionet/models/cultio.py
@@ -68,7 +68,7 @@ class CultioNet(torch.nn.Module):
             self.mask_model = UNet3Psi(
                 in_channels=base_in_channels,
                 out_dist_channels=1,
-                out_edge_channels=2,
+                out_edge_channels=1,
                 out_mask_channels=2,
                 init_filter=self.filters
             )
