f4d3021979a0f2ed851c3cf223d2690b19cb2b07
Jordan Graesser <jgrss@users.noreply.github.com>
2023-01-11 14:49:12 -0600

    v1.6.2

--- src/cultionet/__init__.py
+++ src/cultionet/__init__.py
@@ -1,5 +1,5 @@
 __path__: str = __import__('pkgutil').extend_path(__path__, __name__)
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 from .model import fit, load_model, predict, predict_lightning
 
 __all__ = [
