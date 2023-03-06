2c6cfda14765f89b9d2c34c2a815420dd13a9f55
jgrss <jgraesser@indigoag.com>
2023-03-06 17:32:26 -0300

    test

--- src/cultionet/model.py
+++ src/cultionet/model.py
@@ -639,7 +639,7 @@ def predict_lightning(
     )
 
     trainer = pl.Trainer(**trainer_kwargs)
-    lit_model = CultioLitModel.load_from_checkpoint(
+    cultionet_lit_model = CultioLitModel.load_from_checkpoint(
         checkpoint_path=str(ckpt_file)
     )
 
@@ -648,12 +648,12 @@ def predict_lightning(
         temperature_lit_model = TemperatureScaling.load_from_checkpoint(
             checkpoint_path=str(temperature_ckpt)
         )
-    setattr(lit_model, 'crop_temperature', crop_temperature)
-    setattr(lit_model, 'temperature_lit_model', temperature_lit_model)
+    setattr(cultionet_lit_model, 'crop_temperature', crop_temperature)
+    setattr(cultionet_lit_model, 'temperature_lit_model', temperature_lit_model)
 
     # Make predictions
     trainer.predict(
-        model=lit_model,
+        model=cultionet_lit_model,
         datamodule=data_module,
         return_predictions=False
     )
