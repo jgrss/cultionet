55daad29158463a77c743dd4820e5aca9b6e5a61
jgrss <jgraesser@indigoag.com>
2023-03-08 10:18:27 -0300

    remove StrEnum

--- src/cultionet/models/enums.py
+++ src/cultionet/models/enums.py
@@ -1,11 +1,11 @@
 import enum
 
 
-class ModelTypes(enum.StrEnum):
+class ModelTypes(enum.Enum):
     UNET = enum.auto()
     RESUNET = enum.auto()
 
 
-class ResBlockTypes(enum.StrEnum):
+class ResBlockTypes(enum.Enum):
     RES = enum.auto()
     RESA = enum.auto()
