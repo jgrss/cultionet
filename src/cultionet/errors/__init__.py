4363b1f5c69d1cf8f80d93a1ab673971fd495b68
jgrss <jgraesser@indigoag.com>
2023-03-08 09:46:50 -0300

    format

--- src/cultionet/errors/__init__.py
+++ src/cultionet/errors/__init__.py
@@ -1,12 +1,15 @@
 class TopologyClipError(Exception):
-    """Raised when GeoPandas clip() fails because of topology errors"""
+    """Raised when GeoPandas clip() fails because of topology errors."""
+
     pass
 
 
 class TensorShapeError(Exception):
-    """Raised when tensor shapes do not match
-    """
-    def __init__(self, message: str = 'The tensor shapes do not match.') -> None:
+    """Raised when tensor shapes do not match."""
+
+    def __init__(
+        self, message: str = "The tensor shapes do not match."
+    ) -> None:
         self.message = message
         super().__init__(self.message)
 
