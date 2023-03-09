7e2306f0a6120c2c96cc063a594708e33055bbc8
jgrss <jgraesser@indigoag.com>
2023-03-09 10:28:29 -0300

    format

--- src/cultionet/data/samplers.py
+++ src/cultionet/data/samplers.py
@@ -1,39 +1,36 @@
+from typing import Iterator, Sized
+
 import numpy as np
 from torch.utils.data import Sampler
 
-from .datasets import EdgeDataset
 
-
-class EpochRandomSampler(Sampler):
+class EpochRandomSampler(Sampler[int]):
     """A random sampler per epoch.
 
     Reference:
         Adapted from: https://discuss.pytorch.org/t/new-subset-every-epoch/85018
     """
 
-    def __init__(self, data_source: EdgeDataset, num_samples: int):
-        super().__init__()
+    data_source: Sized
 
+    def __init__(self, data_source: Sized, num_samples: int):
         self.data_source = data_source
         self._num_samples = num_samples
-
-    @property
-    def n(self) -> int:
-        return len(self.data_source)
 
     @property
     def num_samples(self) -> int:
         # dataset size might change at runtime
         if self._num_samples is None:
             return len(self.data_source)
+
         return self._num_samples
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[int]:
         return iter(
             np.random.choice(
-                range(self.n), replace=False, size=self.num_samples
+                range(len(self)), replace=False, size=self.num_samples
             )
         )
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.num_samples
