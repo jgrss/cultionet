2ea5bd19be37a397bfe2c54bb0778688902e2c20
jgrss <jgraesser@indigoag.com>
2023-06-10 12:20:29 +1000

    format

--- src/cultionet/__init__.py
+++ src/cultionet/__init__.py
@@ -1,5 +1,12 @@
 __path__: str = __import__("pkgutil").extend_path(__path__, __name__)
 __version__ = "1.7.2"
-from .model import fit, load_model, predict, predict_lightning
+from .model import fit, fit_transfer, load_model, predict, predict_lightning
 
-__all__ = ["fit", "fit_maskrcnn", "load_model", "predict", "predict_lightning"]
+__all__ = [
+    "fit",
+    "fit_transfer",
+    "fit_maskrcnn",
+    "load_model",
+    "predict",
+    "predict_lightning",
+]
