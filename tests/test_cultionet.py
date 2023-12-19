54b6d8cdf729f7fd2be7341a01dfd719835109db
jgrss <jbgraesser@gmail.com>
2023-12-20 00:46:08 +1100

    update import

--- tests/test_cultionet.py
+++ tests/test_cultionet.py
@@ -1,7 +1,7 @@
 import torch
 
 from cultionet.models import model_utils
-from cultionet.models.base_layers import Softmax
+from cultionet.layers.base_layers import Softmax
 from cultionet.models.nunet import ResUNet3Psi
 from cultionet.models.temporal_attention import TemporalAttention
 
