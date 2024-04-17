ab19a4805e791654d415b146eb684c758be8098b
Jordan Graesser <jgraesser@indigoag.com>
2024-04-17 17:06:32 +1000

    update outputs

--- src/cultionet/models/cultio.py
+++ src/cultionet/models/cultio.py
@@ -8,7 +8,7 @@ from . import model_utils
 from ..layers.base_layers import ConvBlock2d, ResidualConv, Softmax
 from .nunet import UNet3Psi, ResUNet3Psi, ResELUNetPsi
 from .time_attention import TemporalResAUNet
-from .temporal_attention import TemporalAttention
+from .temporal_transformer import TemporalTransformer
 from ..enums import ModelTypes, ResBlockTypes
 
 
@@ -315,12 +315,13 @@ class CultioNet(torch.nn.Module):
         self.cg = model_utils.ConvToGraph()
         self.ct = model_utils.ConvToTime()
 
-        self.temporal_encoder = TemporalAttention(
+        self.temporal_encoder = TemporalTransformer(
             in_channels=self.ds_num_bands,
             hidden_channels=self.filters,
             num_head=8,
             num_time=self.ds_num_time,
             dropout=0.1,
+            num_layers=4,
             d_model=self.filters,
             time_scaler=1_000,
             num_classes_l2=self.num_classes,
@@ -332,7 +333,6 @@ class CultioNet(torch.nn.Module):
         unet3_kwargs = {
             "in_channels": self.ds_num_bands,
             "in_time": self.ds_num_time,
-            "in_encoding_channels": self.filters,  # <- L-TAE; int(self.filters * 3), <- ConvSTAR
             "init_filter": self.filters,
             "num_classes": self.num_classes,
             "activation_type": activation_type,
@@ -381,6 +381,7 @@ class CultioNet(torch.nn.Module):
                 self.mask_model = ResUNet3Psi(**unet3_kwargs)
             else:
                 self.mask_model = ResELUNetPsi(**unet3_kwargs)
+
         elif model_type == ModelTypes.TRESAUNET:
             self.mask_model = TemporalResAUNet(
                 in_channels=self.ds_num_bands,
@@ -407,13 +408,16 @@ class CultioNet(torch.nn.Module):
         x = self.ct(x, nbands=self.ds_num_bands, ntime=self.ds_num_time)
 
         # Transformer attention encoder
-        logits_hidden, classes_l2, classes_last = self.temporal_encoder(x)
+        transformer_outputs = self.temporal_encoder(x)
 
-        classes_l2 = self.cg(classes_l2)
-        classes_last = self.cg(classes_last)
         # Main stream
-        logits = self.mask_model(x, temporal_encoding=logits_hidden)
+        logits = self.mask_model(
+            x,
+            temporal_encoding=transformer_outputs['encoded'],
+        )
 
+        classes_l2 = self.cg(transformer_outputs['l2'])
+        classes_l3 = self.cg(transformer_outputs['l3'])
         logits_distance = self.cg(logits["dist"])
         logits_edges = self.cg(logits["edge"])
         logits_crop = self.cg(logits["mask"])
@@ -424,7 +428,7 @@ class CultioNet(torch.nn.Module):
             "crop": logits_crop,
             "crop_type": None,
             "classes_l2": classes_l2,
-            "classes_last": classes_last,
+            "classes_l3": classes_l3,
         }
 
         if logits["dist_3_1"] is not None:
