8e5be6a8a4a025658f069643db04950fae47f17c
jgrss <jbgraesser@gmail.com>
2024-04-21 19:12:32 +1000

    format

--- src/cultionet/augment/augmenter_utils.py
+++ src/cultionet/augment/augmenter_utils.py
@@ -2,6 +2,7 @@ import typing as T
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from einops import rearrange
 from scipy.ndimage.measurements import label as nd_label
 from tsaug import AddNoise, Drift, TimeWarp
@@ -79,18 +80,27 @@ class SegmentParcel:
         )
 
 
-def reinsert_prop(
-    x: np.ndarray,
-    bounds_slice: tuple,
-    mask: np.ndarray,
-    x_update: np.ndarray,
-    x_original: np.ndarray,
-) -> np.ndarray:
-    x[(slice(0, None),) + bounds_slice] = np.where(
-        mask == 1, x_update, x_original
+def insert_parcel(
+    parcel_data: Data,
+    augmented: torch.Tensor,
+    segment_parcel: SegmentParcel,
+    prop: object,
+) -> Data:
+    parcel_data.x[
+        segment_parcel.coords_slices + segment_parcel.dims_slice
+    ] = torch.where(
+        rearrange(
+            torch.from_numpy(parcel_data.segments)[segment_parcel.dims_slice],
+            'h w -> 1 1 1 h w',
+        )
+        == prop.label,
+        augmented,
+        parcel_data.x[
+            segment_parcel.coords_slices + segment_parcel.dims_slice
+        ],
     )
 
-    return x
+    return parcel_data
 
 
 def augment_time(
@@ -111,16 +121,24 @@ def augment_time(
         width,
     ) = segment_parcel.xseg.shape
 
-    # -> (H*W x T X C)
+    # (1 x C x T x H x W) -> (H*W x T X C)
     xseg = feature_stack_to_tsaug(segment_parcel.xseg)
 
     if aug == "tspeaks":
-        new_indices = np.sort(
-            np.random.choice(
-                range(0, num_time * 2 - 8), replace=False, size=num_time
-            )
+        half_a = F.interpolate(
+            rearrange(xseg, 'b t c -> b c t'),
+            size=num_time // 2,
+            mode='linear',
         )
-        xseg = torch.cat((xseg, xseg), dim=1)[:, 4:-4][:, new_indices]
+        half_b = F.interpolate(
+            rearrange(xseg, 'b t c -> b c t'),
+            size=num_time - half_a.shape[-1],
+            mode='linear',
+        )
+        xseg = rearrange(
+            torch.cat((half_a, half_b), dim=-1),
+            'b c t -> b t c',
+        )
 
     # Warp the segment
     xseg = warper.augment(xseg.numpy())
@@ -134,20 +152,12 @@ def augment_time(
         torch.from_numpy(xseg), height=height, width=width
     ).clip(0, 1)
 
-    # Insert the parcel
-    ldata.x[
-        segment_parcel.coords_slices + segment_parcel.dims_slice
-    ] = torch.where(
-        rearrange(
-            torch.from_numpy(ldata.segments)[segment_parcel.dims_slice],
-            'h w -> 1 1 1 h w',
-        )
-        == p.label,
-        xseg,
-        ldata.x[segment_parcel.coords_slices + segment_parcel.dims_slice],
+    return insert_parcel(
+        parcel_data=ldata,
+        augmented=xseg,
+        segment_parcel=segment_parcel,
+        prop=p,
     )
-
-    return ldata
 
 
 def roll_time(ldata: Data, p: T.Any) -> Data:
@@ -157,86 +167,14 @@ def roll_time(ldata: Data, p: T.Any) -> Data:
     shift = np.random.choice(
         range(-int(ldata.num_time * 0.25), int(ldata.num_time * 0.25) + 1)
     )
-    # Shift time in each band separately
-    for band_idx in range(0, ldata.num_channels):
-        # Get the slice for the current band, n time steps
-        segment_parcel.xseg[0, band_idx] = torch.roll(
-            segment_parcel.xseg[0, band_idx], shift, dims=0
-        )
 
-    # Insert the parcel
-    ldata.x[
-        segment_parcel.coords_slices + segment_parcel.dims_slice
-    ] = torch.where(
-        rearrange(
-            torch.from_numpy(ldata.segments)[segment_parcel.dims_slice],
-            'h w -> 1 1 1 h w',
-        )
-        == p.label,
-        segment_parcel.xseg,
-        ldata.x[segment_parcel.coords_slices + segment_parcel.dims_slice],
+    # Shift time
+    # (1 x C x T x H x W)
+    xseg = torch.roll(segment_parcel.xseg, shift, dims=2)
+
+    return insert_parcel(
+        parcel_data=ldata,
+        augmented=xseg,
+        segment_parcel=segment_parcel,
+        prop=p,
     )
-
-    return ldata
-
-
-def create_parcel_masks(
-    labels_array: np.ndarray, max_crop_class: int
-) -> T.Union[None, dict]:
-    """Creates masks for each instance.
-
-    Reference:
-        https://torchtutorialstaging.z5.web.core.windows.net/intermediate/torchvision_tutorial.html
-    """
-    # Remove edges
-    mask = np.where(
-        (labels_array > 0) & (labels_array <= max_crop_class), 1, 0
-    )
-    mask = nd_label(mask)[0]
-    obj_ids = np.unique(mask)
-    # first id is the background, so remove it
-    obj_ids = obj_ids[1:]
-    # split the color-encoded mask into a set
-    # of binary masks
-    masks = mask == obj_ids[:, None, None]
-
-    # get bounding box coordinates for each mask
-    num_objs = len(obj_ids)
-    boxes = []
-    small_box_idx = []
-    for i in range(num_objs):
-        pos = np.where(masks[i])
-        xmin = np.min(pos[1])
-        xmax = np.max(pos[1])
-        ymin = np.min(pos[0])
-        ymax = np.max(pos[0])
-        # Fields too small
-        if (xmax - xmin == 0) or (ymax - ymin == 0):
-            small_box_idx.append(i)
-            continue
-        boxes.append([xmin, ymin, xmax, ymax])
-
-    if small_box_idx:
-        good_idx = np.array(
-            [
-                idx
-                for idx in range(0, masks.shape[0])
-                if idx not in small_box_idx
-            ]
-        )
-        masks = masks[good_idx]
-    # convert everything into arrays
-    boxes = torch.as_tensor(boxes, dtype=torch.float32)
-    if boxes.size(0) == 0:
-        return None
-    # there is only one class
-    labels = torch.ones((masks.shape[0],), dtype=torch.int64)
-    masks = torch.as_tensor(masks, dtype=torch.uint8)
-
-    assert (
-        boxes.size(0) == labels.size(0) == masks.size(0)
-    ), "The tensor sizes do not match."
-
-    target = {"boxes": boxes, "labels": labels, "masks": masks}
-
-    return target
