df8d9e2516cf9f54a08d81611924dc005b4cfd83
Ubuntu <rbraswell@indigoag.com>
2024-04-23 06:42:47 +0000

    format

--- src/cultionet/data/utils.py
+++ src/cultionet/data/utils.py
@@ -1,28 +1,11 @@
-import shutil
 import typing as T
-from dataclasses import dataclass
 from pathlib import Path
-import uuid
 
 import numpy as np
-import xarray as xr
 import torch
-from torch_geometric.data import Data
+import xarray as xr
 
-from .datasets import EdgeDataset
-from ..networks import SingleSensorNetwork
-from ..utils.reshape import nd_to_columns
-from ..utils.normalize import NormValues
-
-
-@dataclass
-class LabeledData:
-    x: np.ndarray
-    y: T.Union[None, np.ndarray]
-    bdist: T.Union[None, np.ndarray]
-    ori: T.Union[None, np.ndarray]
-    segments: T.Union[None, np.ndarray]
-    props: T.Union[None, T.List]
+from .data import Data
 
 
 def get_image_list_dims(
@@ -122,72 +105,3 @@ def create_data_object(
     train_data.num_nodes = x.shape[0]
 
     return train_data
-
-
-# def create_network_data(xvars: np.ndarray, ntime: int, nbands: int) -> Data:
-#     # Create the network
-#     nwk = SingleSensorNetwork(
-#         np.ascontiguousarray(xvars, dtype="float64"), k=3
-#     )
-
-#     (
-#         edge_indices_a,
-#         edge_indices_b,
-#         edge_attrs_diffs,
-#         edge_attrs_dists,
-#         xpos,
-#         ypos,
-#     ) = nwk.create_network()
-#     edge_indices = np.c_[edge_indices_a, edge_indices_b]
-#     edge_attrs = np.c_[edge_attrs_diffs, edge_attrs_dists]
-#     xy = np.c_[xpos, ypos]
-#     nfeas, nrows, ncols = xvars.shape
-#     xvars = nd_to_columns(xvars, nfeas, nrows, ncols)
-
-#     return create_data_object(
-#         xvars,
-#         xy,
-#         edge_indices=edge_indices,
-#         edge_attrs=edge_attrs,
-#         ntime=ntime,
-#         nbands=nbands,
-#         height=nrows,
-#         width=ncols,
-#     )
-
-
-class NetworkDataset(object):
-    def __init__(self, data: Data, data_path: Path, data_values: NormValues):
-        self.data_values = data_values
-        self.data_path = data_path
-
-        self.processed_path = self.data_path / "processed"
-        self.processed_path.mkdir(parents=True, exist_ok=True)
-
-        # Create a random filename so that the processed
-        # directory can be used by other processes
-        filename = str(uuid.uuid4()).replace("-", "")
-        pt_name = f"{filename}_.pt"
-        self.pattern = f"{filename}*.pt"
-        self.pt_file = self.processed_path / pt_name
-
-        self._save(data)
-
-    def _save(self, data: Data) -> None:
-        torch.save(data, self.pt_file)
-
-    def clear(self) -> None:
-        if self.processed_path.is_dir():
-            shutil.rmtree(str(self.processed_path))
-
-    def unlink(self) -> None:
-        self.pt_file.unlink()
-
-    @property
-    def ds(self) -> EdgeDataset:
-        return EdgeDataset(
-            self.data_path,
-            data_means=self.data_values.mean,
-            data_stds=self.data_values.std,
-            pattern=self.pattern,
-        )
