89162c54e034813316fec308c2281c9cbc241671
jgrss <jbgraesser@gmail.com>
2024-04-25 12:16:42 +1000

    update loss

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,7 +1,8 @@
+import numpy as np
+import pytest
 import torch
 import torch.nn.functional as F
 from einops import rearrange
-from torch.distributions import Dirichlet
 
 from cultionet.losses import (
     LossPreprocessing,
@@ -9,26 +10,40 @@ from cultionet.losses import (
     TanimotoDistLoss,
 )
 
-torch.manual_seed(100)
+rng = np.random.default_rng(100)
+
 BATCH_SIZE = 2
 HEIGHT = 20
 WIDTH = 20
-INPUTS_CROP_LOGIT = torch.randn(BATCH_SIZE, 2, HEIGHT, WIDTH)
+
+INPUTS_CROP_LOGIT = torch.from_numpy(
+    rng.uniform(low=-3, high=3, size=(BATCH_SIZE, 2, HEIGHT, WIDTH))
+).float()
 INPUTS_CROP_PROB = rearrange(
-    Dirichlet(torch.tensor([0.5, 0.5])).rsample(
-        (BATCH_SIZE * HEIGHT * WIDTH,)
-    ),
+    torch.from_numpy(
+        rng.dirichlet((0.5, 0.5), size=(BATCH_SIZE * HEIGHT * WIDTH))
+    ).float(),
     '(b h w) c -> b c h w',
     b=BATCH_SIZE,
     c=2,
     h=HEIGHT,
     w=WIDTH,
 )
-INPUTS_DIST = torch.rand(BATCH_SIZE, 1, HEIGHT, WIDTH)
-DISCRETE_TARGETS = torch.randint(
-    low=0, high=2, size=(BATCH_SIZE, HEIGHT, WIDTH)
-)
-DIST_TARGETS = torch.rand(BATCH_SIZE, HEIGHT, WIDTH)
+INPUTS_EDGE_PROB = torch.from_numpy(
+    rng.random((BATCH_SIZE, 1, HEIGHT, WIDTH))
+).float()
+INPUTS_DIST = torch.from_numpy(
+    rng.random((BATCH_SIZE, 1, HEIGHT, WIDTH))
+).float()
+DISCRETE_TARGETS = torch.from_numpy(
+    rng.integers(low=0, high=2, size=(BATCH_SIZE, HEIGHT, WIDTH))
+).long()
+DISCRETE_EDGE_TARGETS = torch.from_numpy(
+    rng.integers(low=0, high=1, size=(BATCH_SIZE, HEIGHT, WIDTH))
+).long()
+DIST_TARGETS = torch.from_numpy(
+    rng.random((BATCH_SIZE, HEIGHT, WIDTH))
+).float()
 
 
 def test_loss_preprocessing():
@@ -69,12 +84,29 @@ def test_loss_preprocessing():
         rearrange(INPUTS_CROP_PROB, 'b c h w -> (b h w) c'),
     )
 
+    preprocessor = LossPreprocessing(
+        transform_logits=False, one_hot_targets=True
+    )
+    # This should fail because there are more class targets than the input dimensions
+    with pytest.raises(ValueError):
+        inputs, targets = preprocessor(INPUTS_EDGE_PROB, DISCRETE_TARGETS)
+    inputs, targets = preprocessor(INPUTS_EDGE_PROB, DISCRETE_EDGE_TARGETS)
+
+    assert inputs.shape == (BATCH_SIZE * HEIGHT * WIDTH, 1)
+    assert targets.shape == (BATCH_SIZE * HEIGHT * WIDTH, 1)
+    assert torch.allclose(targets.max(dim=0).values, torch.ones(1))
+    assert torch.allclose(
+        inputs,
+        rearrange(INPUTS_EDGE_PROB, 'b c h w -> (b h w) c'),
+    )
+
     # Regression
     preprocessor = LossPreprocessing(
         transform_logits=False, one_hot_targets=False
     )
     inputs, targets = preprocessor(INPUTS_DIST, DIST_TARGETS)
 
+    # Preprocessing should not change the inputs other than the shape
     assert torch.allclose(
         inputs, rearrange(INPUTS_DIST, 'b c h w -> (b h w) c')
     )
@@ -90,11 +122,18 @@ def test_tanimoto_classification_loss():
         one_hot_targets=True,
     )
     loss = loss_func(INPUTS_CROP_PROB, DISCRETE_TARGETS)
-    assert round(float(loss.item()), 4) == 0.6062
+    assert round(float(loss.item()), 3) == 0.611
 
-    loss_func = TanimotoComplementLoss(
-        transform_logits=False,
-        one_hot_targets=True,
-    )
+    loss_func = TanimotoComplementLoss()
     loss = loss_func(INPUTS_CROP_PROB, DISCRETE_TARGETS)
-    assert round(float(loss.item()), 4) == 0.8214
+    assert round(float(loss.item()), 3) == 0.824
+
+
+def test_tanimoto_regression_loss():
+    loss_func = TanimotoDistLoss(one_hot_targets=False)
+    loss = loss_func(INPUTS_DIST, DIST_TARGETS)
+    assert round(float(loss.item()), 4) == 0.4174
+
+    loss_func = TanimotoComplementLoss(one_hot_targets=False)
+    loss = loss_func(INPUTS_DIST, DIST_TARGETS)
+    assert round(float(loss.item()), 3) == 0.704
