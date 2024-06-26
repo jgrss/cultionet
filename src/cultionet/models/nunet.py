ba5fc40720ddc886d33ae952acd0c43793cf9bbd
jgrss <jgraesser@indigoag.com>
2024-06-26 01:04:02 +0000

    move modules

--- src/cultionet/models/nunet.py
+++ src/cultionet/models/nunet.py
@@ -715,6 +715,162 @@ class TowerUNet(nn.Module):
             activation_type=activation_type,
         )
 
+        self.encoder = cunn.TowerUNetEncoder(
+            channels=channels,
+            dilations=dilations,
+            activation_type=activation_type,
+            dropout=dropout,
+            res_block_type=res_block_type,
+            attention_weights=attention_weights,
+            pool_attention=pool_attention,
+            pool_by_max=pool_by_max,
+            repeat_resa_kernel=repeat_resa_kernel,
+            batchnorm_first=batchnorm_first,
+            concat_resid=concat_resid,
+        )
+
+        self.decoder = cunn.TowerUNetDecoder(
+            channels=channels,
+            up_channels=up_channels,
+            dilations=dilations,
+            activation_type=activation_type,
+            dropout=dropout,
+            res_block_type=res_block_type,
+            attention_weights=attention_weights,
+            repeat_resa_kernel=repeat_resa_kernel,
+            batchnorm_first=batchnorm_first,
+            concat_resid=concat_resid,
+        )
+
+        self.tower_decoder = cunn.TowerUNetFusion(
+            channels=channels,
+            up_channels=up_channels,
+            dilations=dilations,
+            activation_type=activation_type,
+            dropout=dropout,
+            res_block_type=res_block_type,
+            attention_weights=attention_weights,
+            repeat_resa_kernel=repeat_resa_kernel,
+            batchnorm_first=batchnorm_first,
+            concat_resid=concat_resid,
+        )
+
+        self.final_a = cunn.TowerUNetFinal(
+            in_channels=up_channels,
+            num_classes=num_classes,
+            mask_activation=mask_activation,
+            activation_type=activation_type,
+        )
+
+        if self.deep_supervision:
+            self.final_b = cunn.TowerUNetFinal(
+                in_channels=up_channels,
+                num_classes=num_classes,
+                mask_activation=mask_activation,
+                activation_type=activation_type,
+                resample_factor=2,
+            )
+            self.final_c = cunn.TowerUNetFinal(
+                in_channels=up_channels,
+                num_classes=num_classes,
+                mask_activation=mask_activation,
+                activation_type=activation_type,
+                resample_factor=4,
+            )
+
+        # Initialize weights
+        self.apply(init_conv_weights)
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        temporal_encoding: T.Optional[torch.Tensor] = None,
+        training: bool = True,
+    ) -> T.Dict[str, torch.Tensor]:
+
+        """Forward pass.
+
+        Parameters
+        ==========
+        x
+            Shaped (B x C x T x H x W)
+        temporal_encoding
+            Shaped (B x C x H X W)
+        """
+
+        # Initial temporal reduction and convolutions to
+        # hidden dimensions
+        embeddings = self.pre_unet(x, temporal_encoding=temporal_encoding)
+
+        encoded = self.encoder(embeddings)
+        decoded = self.decoder(encoded)
+        towers_decoded = self.tower_decoder(encoded=encoded, decoded=decoded)
+
+        # Final outputs
+        out = self.final_a(towers_decoded["x_tower_a"])
+
+        if training and self.deep_supervision:
+            out_c = self.final_c(
+                towers_decoded["x_tower_c"],
+                size=towers_decoded["x_tower_a"].shape[-2:],
+                suffix="_c",
+            )
+            out_b = self.final_b(
+                towers_decoded["x_tower_b"],
+                size=towers_decoded["x_tower_a"].shape[-2:],
+                suffix="_b",
+            )
+
+            out.update(out_b)
+            out.update(out_c)
+
+        return out
+
+
+class _TowerUNet(nn.Module):
+    """Tower U-Net."""
+
+    def __init__(
+        self,
+        in_channels: int,
+        in_time: int,
+        hidden_channels: int = 64,
+        num_classes: int = 2,
+        dilations: T.Sequence[int] = None,
+        activation_type: str = "SiLU",
+        dropout: float = 0.0,
+        res_block_type: str = ResBlockTypes.RESA,
+        attention_weights: str = AttentionTypes.SPATIAL_CHANNEL,
+        mask_activation: T.Union[nn.Softmax, nn.Sigmoid] = nn.Softmax(dim=1),
+        deep_supervision: bool = False,
+        pool_attention: bool = False,
+        pool_by_max: bool = False,
+        repeat_resa_kernel: bool = False,
+        batchnorm_first: bool = False,
+        concat_resid: bool = False,
+    ):
+        super(TowerUNet, self).__init__()
+
+        if dilations is None:
+            dilations = [1, 2]
+
+        self.deep_supervision = deep_supervision
+
+        channels = [
+            hidden_channels,  # a
+            hidden_channels * 2,  # b
+            hidden_channels * 4,  # c
+            hidden_channels * 8,  # d
+        ]
+        up_channels = int(hidden_channels * len(channels))
+
+        self.pre_unet = PreUnet3Psi(
+            in_channels=in_channels,
+            in_time=in_time,
+            out_channels=channels[0],
+            activation_type=activation_type,
+        )
+
         # Backbone layers
         backbone_kwargs = dict(
             dropout=dropout,
