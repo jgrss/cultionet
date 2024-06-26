6b4755e1cedde5dd41177a40898911d481961fd8
jgrss <jgraesser@indigoag.com>
2024-06-26 01:01:09 +0000

    simplify augmenter

--- src/cultionet/augment/augmenters.py
+++ src/cultionet/augment/augmenters.py
@@ -1,6 +1,6 @@
 import typing as T
 from abc import abstractmethod
-from dataclasses import dataclass, replace
+from dataclasses import replace
 from pathlib import Path
 
 import einops
@@ -16,29 +16,17 @@ from ..data.data import Data
 from .augmenter_utils import augment_time, generate_perlin_noise_3d, roll_time
 
 
-@dataclass
-class DataCopies:
-    x: torch.Tensor
-    y: T.Union[torch.Tensor, None]
-    bdist: T.Union[torch.Tensor, None]
-
-
-@dataclass
-class AugmenterArgs:
-    kwargs: dict
-
-
 class AugmenterModule:
     """Prepares, augments, and finalizes data."""
 
     prefix: str = "data_"
     suffix: str = ".pt"
 
-    def __call__(self, ldata: Data, aug_args: AugmenterArgs) -> Data:
+    def __call__(self, ldata: Data) -> Data:
         assert hasattr(self, "name_")
         assert isinstance(self.name_, str)
 
-        cdata = self.forward(ldata.copy(), aug_args)
+        cdata = self.forward(ldata.copy())
         cdata.x = cdata.x.float().clip(1e-9, 1)
         cdata.bdist = cdata.bdist.float().clip(0, 1)
         if cdata.y is not None:
@@ -47,7 +35,7 @@ class AugmenterModule:
         return cdata
 
     @abstractmethod
-    def forward(self, cdata: Data, aug_args: AugmenterArgs) -> Data:
+    def forward(self, cdata: Data) -> Data:
         raise NotImplementedError
 
     def file_name(self, uid: str) -> str:
@@ -61,7 +49,7 @@ class AugmenterModule:
 
 
 class AugmentTimeMixin(AugmenterModule):
-    def forward(self, cdata: Data, aug_args: AugmenterArgs) -> Data:
+    def forward(self, cdata: Data) -> Data:
         # Warp each segment
         for p in cdata.props:
             cdata = augment_time(
@@ -81,10 +69,12 @@ class AugmentTimeWarp(AugmentTimeMixin):
         name: str,
         n_speed_change_lim: T.Tuple[int, int] = None,
         max_speed_ratio_lim: T.Tuple[float, float] = None,
+        rng: T.Optional[np.random.Generator] = None,
     ):
+        self.name_ = name
         self.n_speed_change_lim = n_speed_change_lim
         self.max_speed_ratio_lim = max_speed_ratio_lim
-        self.name_ = name
+        self.rng = rng
         self.add_noise_ = True
 
         if self.n_speed_change_lim is None:
@@ -93,10 +83,13 @@ class AugmentTimeWarp(AugmentTimeMixin):
             self.max_speed_ratio_lim = (1.1, 1.5)
 
         self.warper = TimeWarp(
-            n_speed_change=np.random.randint(
-                low=self.n_speed_change_lim[0], high=self.n_speed_change_lim[1]
+            n_speed_change=int(
+                self.rng.integers(
+                    low=self.n_speed_change_lim[0],
+                    high=self.n_speed_change_lim[1],
+                )
             ),
-            max_speed_ratio=np.random.uniform(
+            max_speed_ratio=self.rng.uniform(
                 low=self.max_speed_ratio_lim[0],
                 high=self.max_speed_ratio_lim[1],
             ),
@@ -105,8 +98,13 @@ class AugmentTimeWarp(AugmentTimeMixin):
 
 
 class AugmentAddTimeNoise(AugmentTimeMixin):
-    def __init__(self, scale_lim: T.Tuple[int, int] = None):
+    def __init__(
+        self,
+        scale_lim: T.Tuple[int, int] = None,
+        rng: T.Optional[np.random.Generator] = None,
+    ):
         self.scale_lim = scale_lim
+        self.rng = rng
         self.name_ = "tsnoise"
         self.add_noise_ = False
 
@@ -114,7 +112,7 @@ class AugmentAddTimeNoise(AugmentTimeMixin):
             self.scale_lim = (0.01, 0.05)
 
         self.warper = AddNoise(
-            scale=np.random.uniform(
+            scale=self.rng.uniform(
                 low=self.scale_lim[0], high=self.scale_lim[1]
             )
         )
@@ -125,9 +123,11 @@ class AugmentTimeDrift(AugmentTimeMixin):
         self,
         max_drift_lim: T.Tuple[int, int] = None,
         n_drift_points_lim: T.Tuple[int, int] = None,
+        rng: T.Optional[np.random.Generator] = None,
     ):
         self.max_drift_lim = max_drift_lim
         self.n_drift_points_lim = n_drift_points_lim
+        self.rng = rng
         self.name_ = "tsdrift"
         self.add_noise_ = True
 
@@ -137,66 +137,61 @@ class AugmentTimeDrift(AugmentTimeMixin):
             self.n_drift_points_lim = (1, 6)
 
         self.warper = Drift(
-            max_drift=np.random.uniform(
-                low=self.max_drift_lim[0], high=self.max_drift_lim[1]
+            max_drift=self.rng.uniform(
+                low=self.max_drift_lim[0],
+                high=self.max_drift_lim[1],
             ),
-            n_drift_points=np.random.randint(
-                low=self.n_drift_points_lim[0], high=self.n_drift_points_lim[1]
+            n_drift_points=int(
+                self.rng.integers(
+                    low=self.n_drift_points_lim[0],
+                    high=self.n_drift_points_lim[1],
+                )
             ),
             static_rand=True,
         )
 
 
 class Roll(AugmenterModule):
-    def __init__(self):
+    def __init__(self, rng: T.Optional[np.random.Generator] = None):
+        self.rng = rng
         self.name_ = "roll"
 
-    def forward(
-        self,
-        cdata: Data,
-        aug_args: AugmenterArgs = None,
-    ) -> Data:
+    def forward(self, cdata: Data) -> Data:
         for p in cdata.props:
-            cdata = roll_time(cdata, p)
+            cdata = roll_time(cdata, p, rng=self.rng)
 
         return cdata
 
 
 class PerlinNoise(AugmenterModule):
-    def __init__(self):
+    def __init__(self, rng: T.Optional[np.random.Generator] = None):
+        self.rng = rng
         self.name_ = "perlin"
 
-    def forward(
-        self,
-        cdata: Data,
-        aug_args: AugmenterArgs = None,
-    ) -> Data:
-        res = aug_args.rng.choice([2, 5, 10])
+    def forward(self, cdata: Data) -> Data:
+        res = self.rng.choice([2, 5, 10])
         noise = generate_perlin_noise_3d(
             shape=cdata.x.shape[2:],
             res=(1, res, res),
             tileable=(False, False, False),
             out_range=(-0.03, 0.03),
-            rng=aug_args.rng,
+            rng=self.rng,
         )
 
         noise = einops.rearrange(noise, 't h w -> 1 1 t h w')
-        cdata.x = cdata.x + noise
+        cdata.x = cdata.x + noise.to(
+            dtype=cdata.x.dtype, device=cdata.x.device
+        )
 
         return cdata
 
 
 class Rotate(AugmenterModule):
-    def __init__(self, deg: int):
-        self.name_ = f"rotate-{deg}"
+    def __init__(self, deg: int, **kwargs):
         self.deg = deg
+        self.name_ = f"rotate-{deg}"
 
-    def forward(
-        self,
-        cdata: Data,
-        aug_args: AugmenterArgs = None,
-    ) -> Data:
-
+    def forward(self, cdata: Data) -> Data:
         x = einops.rearrange(cdata.x, '1 c t h w -> 1 t c h w')
 
         x_rotation_transform = v2.RandomRotation(
@@ -219,15 +214,11 @@ class Rotate(AugmenterModule):
 
 
 class Flip(AugmenterModule):
-    def __init__(self, direction: str):
+    def __init__(self, direction: str, **kwargs):
         self.direction = direction
         self.name_ = direction
 
-    def forward(
-        self,
-        cdata: Data,
-        aug_args: AugmenterArgs = None,
-    ) -> Data:
+    def forward(self, cdata: Data) -> Data:
         x = einops.rearrange(cdata.x, '1 c t h w -> 1 t c h w')
 
         if self.direction == 'fliplr':
@@ -248,20 +239,15 @@ class Flip(AugmenterModule):
 
 
 class RandomCropResize(AugmenterModule):
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
+    def __init__(self, rng: T.Optional[np.random.Generator] = None):
+        self.rng = rng
         self.name_ = "cropresize"
 
-    def forward(
-        self,
-        cdata: Data,
-        aug_args: AugmenterArgs = None,
-    ) -> DataCopies:
-
-        div = aug_args.rng.choice([2, 4])
+    def forward(self, cdata: Data) -> Data:
+        div = self.rng.choice([2, 4])
         size = (cdata.y.shape[-2] // div, cdata.y.shape[-1] // div)
 
-        random_seed = aug_args.rng.integers(low=0, high=2147483647)
+        random_seed = self.rng.integers(low=0, high=2147483647)
 
         x = einops.rearrange(cdata.x, 'b c t h w -> b t c h w')
         x = self.random_crop(
@@ -308,15 +294,11 @@ class RandomCropResize(AugmenterModule):
 
 
 class GaussianBlur(AugmenterModule):
-    def __init__(self, **kwargs):
+    def __init__(self, rng: T.Optional[np.random.Generator] = None, **kwargs):
         self.kwargs = kwargs
         self.name_ = "gaussian"
 
-    def forward(
-        self,
-        cdata: Data,
-        aug_args: AugmenterArgs = None,
-    ) -> DataCopies:
+    def forward(self, cdata: Data) -> Data:
         transform = v2.GaussianBlur(kernel_size=3, **self.kwargs)
         cdata.x = transform(cdata.x)
 
@@ -324,78 +306,76 @@ class GaussianBlur(AugmenterModule):
 
 
 class SaltAndPepperNoise(AugmenterModule):
-    def __init__(self, **kwargs):
+    def __init__(self, rng: T.Optional[np.random.Generator] = None, **kwargs):
+        self.rng = rng
         self.kwargs = kwargs
         self.name_ = "s&p"
 
-    def forward(
-        self,
-        cdata: Data,
-        aug_args: AugmenterArgs = None,
-    ) -> DataCopies:
-        cdata.x = self.gaussian_noise(cdata.x, **self.kwargs)
+    def forward(self, cdata: Data) -> Data:
+        random_seed = self.rng.integers(low=0, high=2147483647)
+        cdata.x = self.gaussian_noise(
+            cdata.x,
+            random_seed=random_seed,
+            **self.kwargs,
+        )
 
         return cdata
 
     def gaussian_noise(
-        self, x: torch.Tensor, sigma: float = 0.01
+        self, x: torch.Tensor, random_seed: int, sigma: float = 0.01
     ) -> torch.Tensor:
+        np.random.seed(random_seed)
+        torch.manual_seed(random_seed)
+
         return x + sigma * torch.randn_like(x)
 
 
 class NoAugmentation(AugmenterModule):
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.name_ = "none"
 
-    def forward(
-        self,
-        cdata: DataCopies,
-        ldata: Data = None,
-        aug_args: AugmenterArgs = None,
-    ) -> DataCopies:
+    def forward(self, cdata: Data) -> Data:
         return cdata
 
 
 AUGMENTER_METHODS = frozendict(
-    tswarp=AugmentTimeWarp(name="tswarp"),
-    tsnoise=AugmentAddTimeNoise(),
-    tsdrift=AugmentTimeDrift(),
-    tspeaks=AugmentTimeWarp(name="tspeaks"),
-    rot90=Rotate(deg=90),
-    rot180=Rotate(deg=180),
-    rot270=Rotate(deg=270),
-    roll=Roll(),
-    fliplr=Flip(direction="fliplr"),
-    flipud=Flip(direction="flipud"),
-    gaussian=GaussianBlur(sigma=(0.2, 0.5)),
-    saltpepper=SaltAndPepperNoise(sigma=0.01),
-    cropresize=RandomCropResize(),
-    perlin=PerlinNoise(),
-    none=NoAugmentation(),
+    tswarp=AugmentTimeWarp,
+    tsnoise=AugmentAddTimeNoise,
+    tsdrift=AugmentTimeDrift,
+    tspeaks=AugmentTimeWarp,
+    rot90=Rotate,
+    rot180=Rotate,
+    rot270=Rotate,
+    roll=Roll,
+    fliplr=Flip,
+    flipud=Flip,
+    gaussian=GaussianBlur,
+    saltpepper=SaltAndPepperNoise,
+    cropresize=RandomCropResize,
+    perlin=PerlinNoise,
+    none=NoAugmentation,
+)
+
+MODULE_DEFAULTS = dict(
+    tswarp=dict(name="tswarp"),
+    tsnoise={},
+    tsdrift={},
+    tspeaks=dict(name="tspeaks"),
+    rot90=dict(deg=90),
+    rot180=dict(deg=180),
+    rot270=dict(deg=270),
+    roll={},
+    fliplr=dict(direction="fliplr"),
+    flipud=dict(direction="flipud"),
+    gaussian=dict(sigma=(0.2, 0.5)),
+    saltpepper=dict(sigma=0.01),
+    cropresize={},
+    perlin={},
+    none={},
 )
 
 
-class AugmenterBase:
-    def __init__(
-        self,
-        augmentations: T.Sequence[str],
-        **kwargs,
-    ):
-        self.augmentations = augmentations
-        self.augmenters_ = []
-        self.aug_args = AugmenterArgs(kwargs=kwargs)
-
-        self._init_augmenters()
-
-    def _init_augmenters(self):
-        for augmentation in self.augmentations:
-            self.augmenters_.append(AUGMENTER_METHODS[augmentation])
-
-    def update_aug_args(self, **kwargs):
-        self.aug_args = replace(self.aug_args, **kwargs)
-
-
-class Augmenters(AugmenterBase):
+class Augmenters:
     """Applies augmentations for a sequence of augmentation methods.
 
     Inputs to callables:
@@ -421,17 +401,45 @@ class Augmenters(AugmenterBase):
         >>> ldata = augmenters(ldata)
     """
 
-    def __init__(self, **kwargs):
-        super(Augmenters, self).__init__(**kwargs)
+    def __init__(
+        self,
+        augmentations: T.Sequence[str],
+        rng: T.Optional[np.random.Generator] = None,
+        random_seed: T.Optional[int] = None,
+        **kwargs,
+    ):
+        self.augmentations = augmentations
+        self.augmenters_ = []
+        self.kwargs = kwargs
+
+        if rng is None:
+            rng = np.random.default_rng(random_seed)
+
+        self._init_augmenters(rng)
+
+    def _init_augmenters(self, rng: np.random.Generator):
+        for aug_name in self.augmentations:
+            self.augmenters_.append(
+                AUGMENTER_METHODS[aug_name](
+                    **{
+                        "rng": rng,
+                        **MODULE_DEFAULTS[aug_name],
+                        **self.kwargs,
+                    }
+                )
+            )
+
+    def update_aug_args(self, **kwargs):
+        self.aug_args = replace(self.aug_args, **kwargs)
 
     def __iter__(self):
         yield from self.augmenters_
 
     def __call__(self, batch: Data) -> Data:
-        return self.apply(batch)
+        return self.forward(batch)
 
-    def apply(self, batch: Data) -> Data:
+    def forward(self, batch: Data) -> Data:
         for augmenter in self:
-            batch = augmenter(batch, aug_args=self.aug_args)
+            batch = augmenter(batch)
 
         return batch
