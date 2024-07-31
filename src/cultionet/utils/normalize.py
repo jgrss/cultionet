a933700e1e67fac9e9b3e3e536363382e6348fca
Jordan Graesser <jgraesser@indigoag.com>
2024-07-31 17:10:09 +1000

    🎨 formatting

--- src/cultionet/utils/normalize.py
+++ src/cultionet/utils/normalize.py
@@ -28,6 +28,8 @@ def add_dim(d: torch.Tensor) -> torch.Tensor:
 
 
 class NormValues:
+    """Normalization values."""
+
     def __init__(
         self,
         dataset_mean: torch.Tensor,
@@ -131,199 +133,145 @@ class NormValues:
         lower_bound = None
         upper_bound = None
 
-        if not isinstance(dataset, Dataset):
-            data_loader = DataLoader(
-                dataset,
-                batch_size=batch_size,
-                shuffle=False,
-                num_workers=0,
-                collate_fn=collate_fn,
-            )
+        data_loader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=False,
+            collate_fn=collate_fn,
+        )
 
-            data_maxs = torch.zeros(3, dtype=torch.float)
-            data_sums = torch.zeros(3, dtype=torch.float)
-            sse = torch.zeros(3, dtype=torch.float)
-            pix_count = 0.0
-            with tqdm(
-                total=int(len(dataset) / batch_size),
-                desc='Calculating means',
-                colour=mean_color,
-            ) as pbar:
-                for x, y in data_loader:
-                    channel_maxs = torch.tensor(
-                        [x[0, c, ...].max() for c in range(0, x.shape[1])],
-                        dtype=torch.float,
-                    )
-                    data_maxs = torch.where(
-                        channel_maxs > data_maxs, channel_maxs, data_maxs
-                    )
-                    # Sum over all data
-                    data_sums += x.sum(dim=(0, 2, 3))
-                    pix_count += x.shape[2] * x.shape[3]
+        if centering == 'median':
+            stat_var = Variance(method='median')
+            stat_q = Quantile(r=1024 * 6)
+            tmp_cache_path = Path.home().absolute() / '.cultionet'
+            tmp_cache_path.mkdir(parents=True, exist_ok=True)
+            var_data_cache = tmp_cache_path / '_var.npz'
+            q_data_cache = tmp_cache_path / '_q.npz'
+            crop_counts = torch.zeros(class_info['max_crop_class'] + 1).long()
+            edge_counts = torch.zeros(2).long()
+            with cache_load_enabled(True):
+                with Progress(
+                    TextColumn(
+                        "Calculating stats", style=Style(color="#cacaca")
+                    ),
+                    TextColumn("•", style=Style(color="#cacaca")),
+                    BarColumn(
+                        style="#ACFCD6",
+                        complete_style="#AA9439",
+                        finished_style="#ACFCD6",
+                        pulse_style="#FCADED",
+                    ),
+                    TaskProgressColumn(),
+                    TextColumn("•", style=Style(color="#cacaca")),
+                    TimeElapsedColumn(),
+                ) as pbar:
+                    for batch in pbar.track(
+                        tally_stats(
+                            stats=(stat_var, stat_q),
+                            loader=data_loader,
+                            caches=(var_data_cache, q_data_cache),
+                        ),
+                        total=len(data_loader),
+                    ):
+                        # Stack samples
+                        x = rearrange(batch.x, 'b c t h w -> (b t h w) c')
 
-                    pbar.update(1)
+                        # Update the stats
+                        stat_var.add(x)
+                        stat_q.add(x)
 
-            data_means = data_sums / float(pix_count)
-            with tqdm(
-                total=int(len(dataset) / batch_size),
-                desc='Calculating SSEs',
-                colour=sse_color,
-            ) as pbar:
-                for x, y in data_loader:
-                    sse += (
-                        (x - data_means.unsqueeze(0)[..., None, None]).pow(2)
-                    ).sum(dim=(0, 2, 3))
+                        # Update counts
+                        crop_counts[0] += (
+                            (batch.y == 0)
+                            | (batch.y == class_info['edge_class'])
+                        ).sum()
+                        for i in range(1, class_info['edge_class']):
+                            crop_counts[i] += (batch.y == i).sum()
 
-                    pbar.update(1)
+                        edge_counts[0] += (
+                            (batch.y >= 0)
+                            & (batch.y != class_info['edge_class'])
+                        ).sum()
+                        edge_counts[1] += (
+                            batch.y == class_info['edge_class']
+                        ).sum()
 
-            data_stds = torch.sqrt(sse / pix_count)
+            data_stds = stat_var.std()
+            data_means = stat_q.median()
+            lower_bound = stat_q.quantiles(0.3)
+            upper_bound = stat_q.quantiles(0.7)
+
+            var_data_cache.unlink()
+            q_data_cache.unlink()
+            tmp_cache_path.rmdir()
 
         else:
-            data_loader = DataLoader(
-                dataset,
-                batch_size=batch_size,
-                num_workers=num_workers,
-                shuffle=False,
-                collate_fn=collate_fn,
+
+            def get_info(
+                x: torch.Tensor, y: torch.Tensor
+            ) -> T.Tuple[torch.Tensor, int, torch.Tensor, torch.Tensor]:
+                crop_counts = torch.zeros(class_info['max_crop_class'] + 1)
+                edge_counts = torch.zeros(2)
+                crop_counts[0] = (
+                    (y == 0) | (y == class_info['edge_class'])
+                ).sum()
+                for i in range(1, class_info['edge_class']):
+                    crop_counts[i] = (y == i).sum()
+                edge_counts[0] = (y != class_info['edge_class']).sum()
+                edge_counts[1] = (y == class_info['edge_class']).sum()
+
+                return x.sum(dim=0), x.shape[0], crop_counts, edge_counts
+
+            with parallel_backend(
+                backend='loky',
+                n_jobs=processes,
+                inner_max_num_threads=threads_per_worker,
+            ):
+                with ParallelProgress(
+                    tqdm_kwargs={
+                        'total': int(len(dataset) / batch_size),
+                        'desc': 'Calculating means',
+                        'colour': mean_color,
+                    }
+                ) as pool:
+                    results = pool(
+                        delayed(get_info)(batch.x, batch.y)
+                        for batch in data_loader
+                    )
+            data_sums, pix_count, crop_counts, edge_counts = list(
+                map(list, zip(*results))
             )
 
-            if centering == 'median':
-                stat_var = Variance(method='median')
-                stat_q = Quantile(r=1024 * 6)
-                tmp_cache_path = Path.home().absolute() / '.cultionet'
-                tmp_cache_path.mkdir(parents=True, exist_ok=True)
-                var_data_cache = tmp_cache_path / '_var.npz'
-                q_data_cache = tmp_cache_path / '_q.npz'
-                crop_counts = torch.zeros(
-                    class_info['max_crop_class'] + 1
-                ).long()
-                edge_counts = torch.zeros(2).long()
-                with cache_load_enabled(True):
-                    with Progress(
-                        TextColumn(
-                            "Calculating stats", style=Style(color="#cacaca")
-                        ),
-                        TextColumn("•", style=Style(color="#cacaca")),
-                        BarColumn(
-                            style="#ACFCD6",
-                            complete_style="#AA9439",
-                            finished_style="#ACFCD6",
-                            pulse_style="#FCADED",
-                        ),
-                        TaskProgressColumn(),
-                        TextColumn("•", style=Style(color="#cacaca")),
-                        TimeElapsedColumn(),
-                    ) as pbar:
-                        for batch in pbar.track(
-                            tally_stats(
-                                stats=(stat_var, stat_q),
-                                loader=data_loader,
-                                caches=(var_data_cache, q_data_cache),
-                            ),
-                            total=len(data_loader),
-                        ):
-                            # Stack samples
-                            x = rearrange(batch.x, 'b c t h w -> (b t h w) c')
+            data_sums = torch.stack(data_sums).sum(dim=0)
+            pix_count = torch.tensor(pix_count).sum()
+            crop_counts = torch.stack(crop_counts).sum(dim=0)
+            edge_counts = torch.stack(edge_counts).sum(dim=0)
+            data_means = data_sums / float(pix_count)
 
-                            # Update the stats
-                            stat_var.add(x)
-                            stat_q.add(x)
+            def get_sse(x_mu: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
+                return ((x - x_mu).pow(2)).sum(dim=0)
 
-                            # Update counts
-                            crop_counts[0] += (
-                                (batch.y == 0)
-                                | (batch.y == class_info['edge_class'])
-                            ).sum()
-                            for i in range(1, class_info['edge_class']):
-                                crop_counts[i] += (batch.y == i).sum()
+            sse_partial = partial(get_sse, add_dim(data_means))
 
-                            edge_counts[0] += (
-                                (batch.y >= 0)
-                                & (batch.y != class_info['edge_class'])
-                            ).sum()
-                            edge_counts[1] += (
-                                batch.y == class_info['edge_class']
-                            ).sum()
+            with parallel_backend(
+                backend='loky',
+                n_jobs=processes,
+                inner_max_num_threads=threads_per_worker,
+            ):
+                with ParallelProgress(
+                    tqdm_kwargs={
+                        'total': int(len(dataset) / batch_size),
+                        'desc': 'Calculating SSEs',
+                        'colour': sse_color,
+                    }
+                ) as pool:
+                    sses = pool(
+                        delayed(sse_partial)(batch.x) for batch in data_loader
+                    )
 
-                data_stds = stat_var.std()
-                data_means = stat_q.median()
-                lower_bound = stat_q.quantiles(0.3)
-                upper_bound = stat_q.quantiles(0.7)
-
-                var_data_cache.unlink()
-                q_data_cache.unlink()
-                tmp_cache_path.rmdir()
-
-            else:
-
-                def get_info(
-                    x: torch.Tensor, y: torch.Tensor
-                ) -> T.Tuple[torch.Tensor, int, torch.Tensor, torch.Tensor]:
-                    crop_counts = torch.zeros(class_info['max_crop_class'] + 1)
-                    edge_counts = torch.zeros(2)
-                    crop_counts[0] = (
-                        (y == 0) | (y == class_info['edge_class'])
-                    ).sum()
-                    for i in range(1, class_info['edge_class']):
-                        crop_counts[i] = (y == i).sum()
-                    edge_counts[0] = (y != class_info['edge_class']).sum()
-                    edge_counts[1] = (y == class_info['edge_class']).sum()
-
-                    return x.sum(dim=0), x.shape[0], crop_counts, edge_counts
-
-                with parallel_backend(
-                    backend='loky',
-                    n_jobs=processes,
-                    inner_max_num_threads=threads_per_worker,
-                ):
-                    with ParallelProgress(
-                        tqdm_kwargs={
-                            'total': int(len(dataset) / batch_size),
-                            'desc': 'Calculating means',
-                            'colour': mean_color,
-                        }
-                    ) as pool:
-                        results = pool(
-                            delayed(get_info)(batch.x, batch.y)
-                            for batch in data_loader
-                        )
-                data_sums, pix_count, crop_counts, edge_counts = list(
-                    map(list, zip(*results))
-                )
-
-                data_sums = torch.stack(data_sums).sum(dim=0)
-                pix_count = torch.tensor(pix_count).sum()
-                crop_counts = torch.stack(crop_counts).sum(dim=0)
-                edge_counts = torch.stack(edge_counts).sum(dim=0)
-                data_means = data_sums / float(pix_count)
-
-                def get_sse(
-                    x_mu: torch.Tensor, x: torch.Tensor
-                ) -> torch.Tensor:
-                    return ((x - x_mu).pow(2)).sum(dim=0)
-
-                sse_partial = partial(get_sse, add_dim(data_means))
-
-                with parallel_backend(
-                    backend='loky',
-                    n_jobs=processes,
-                    inner_max_num_threads=threads_per_worker,
-                ):
-                    with ParallelProgress(
-                        tqdm_kwargs={
-                            'total': int(len(dataset) / batch_size),
-                            'desc': 'Calculating SSEs',
-                            'colour': sse_color,
-                        }
-                    ) as pool:
-                        sses = pool(
-                            delayed(sse_partial)(batch.x)
-                            for batch in data_loader
-                        )
-
-                sses = torch.stack(sses).sum(dim=0)
-                data_stds = torch.sqrt(sses / float(pix_count))
-                data_maxs = torch.zeros_like(data_means)
+            sses = torch.stack(sses).sum(dim=0)
+            data_stds = torch.sqrt(sses / float(pix_count))
 
         return cls(
             dataset_mean=rearrange(data_means, 'c -> 1 c 1 1 1'),
