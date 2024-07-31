e5950b9f365bcf360ea29b82dfd9625489e4540d
Jordan Graesser <jgraesser@indigoag.com>
2024-07-31 18:30:28 +1000

    🔥 py3 super() and remove unused methods

--- src/cultionet/layers/encodings.py
+++ src/cultionet/layers/encodings.py
@@ -28,18 +28,3 @@ def get_sinusoid_encoding_table(
     sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
 
     return torch.tensor(sinusoid_table, dtype=torch.float32)
-
-
-def cartesian(lon: torch.Tensor, lat: torch.Tensor) -> torch.Tensor:
-    """
-    Source:
-        https://github.com/nasaharvest/presto/blob/main/presto/presto.py
-    """
-    with torch.no_grad():
-        lon_rad = torch.deg2rad(lon)
-        lat_rad = torch.deg2rad(lat)
-        x = torch.cos(lat_rad) * torch.cos(lon_rad)
-        y = torch.cos(lat_rad) * torch.sin(lon_rad)
-        z = torch.sin(lat_rad)
-
-    return torch.stack([x, y, z], dim=-1)
