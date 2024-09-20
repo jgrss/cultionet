2f34ab36bb97e5fdd491daba80827d6eabacd805
jgrss <jgraesser@indigoag.com>
2024-09-20 04:38:12 +0000

    ⚡️ relative loss imports

--- src/cultionet/losses/__init__.py
+++ src/cultionet/losses/__init__.py
@@ -2,7 +2,11 @@ from .losses import (
     BoundaryLoss,
     ClassBalancedMSELoss,
     CombinedLoss,
+    CrossEntropyLoss,
+    FocalTverskyLoss,
+    LogCoshLoss,
     LossPreprocessing,
     TanimotoComplementLoss,
     TanimotoDistLoss,
+    TverskyLoss,
 )
