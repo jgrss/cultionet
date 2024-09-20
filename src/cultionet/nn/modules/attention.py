08e4afcec1454bfd8e928f11965e762d037d04eb
jgrss <jgraesser@indigoag.com>
2024-09-20 04:42:56 +0000

    🎨

--- src/cultionet/nn/modules/attention.py
+++ src/cultionet/nn/modules/attention.py
@@ -103,10 +103,8 @@ class AttentionGate(nn.Module):
         return self.final(x * h)
 
 
-class TanimotoComplement(nn.Module):
-    """Tanimoto distance with complement.
-
-    THIS IS NOT CURRENTLY USED ANYWHERE IN THIS REPOSITORY
+class FractalTanimoto(nn.Module):
+    """Average Tanimoto.
 
     Adapted from publications and source code below:
 
@@ -133,156 +131,71 @@ class TanimotoComplement(nn.Module):
         References:
             https://www.mdpi.com/2072-4292/14/22/5738
             https://arxiv.org/abs/2009.02062
-            https://github.com/waldnerf/decode/blob/main/FracTAL_ResUNet/nn/layers/ftnmt.py
+            https://github.com/waldnerf/decode/blob/bed6f6b4173f49362a5113207155cc103e8fd139/FracTAL_ResUNet/nn/layers/ftnmt.py
     """
 
     def __init__(
         self,
         smooth: float = 1e-5,
         depth: int = 5,
-        dim: T.Union[int, T.Sequence[int]] = 0,
-        targets_are_labels: bool = True,
+        dim: T.Optional[T.Tuple[int, ...]] = None,
     ):
         super().__init__()
 
         self.smooth = smooth
         self.depth = depth
         self.dim = dim
-        self.targets_are_labels = targets_are_labels
 
-    def forward(
-        self, inputs: torch.Tensor, targets: torch.Tensor
-    ) -> torch.Tensor:
-        """Performs a single forward pass.
+        if self.dim is None:
+            self.dim = (1, 2, 3)
 
-        Args:
-            inputs: Predictions from model (probabilities or labels).
-            targets: Ground truth values.
-
-        Returns:
-            Tanimoto distance loss (float)
-        """
-        if self.depth == 1:
-            scale = 1.0
-        else:
-            scale = 1.0 / self.depth
-
-        def tanimoto(y: torch.Tensor, yhat: torch.Tensor) -> torch.Tensor:
-            tpl = torch.sum(y * yhat, dim=self.dim, keepdim=True)
-            numerator = tpl + self.smooth
-            sq_sum = torch.sum(y**2 + yhat**2, dim=self.dim, keepdim=True)
-            denominator = torch.zeros(1, dtype=inputs.dtype).to(
-                device=inputs.device
-            )
-            for d in range(0, self.depth):
-                a = 2**d
-                b = -(2.0 * a - 1.0)
-                denominator = denominator + torch.reciprocal(
-                    (a * sq_sum) + (b * tpl) + self.smooth
-                )
-
-            return numerator * denominator * scale
-
-        l1 = tanimoto(targets, inputs)
-        l2 = tanimoto(1.0 - targets, 1.0 - inputs)
-        score = (l1 + l2) * 0.5
-
-        return score
-
-
-class TanimotoDist(nn.Module):
-    r"""Tanimoto distance.
-
-    Adapted from publication and source code below:
-
-        CSIRO BSTD/MIT LICENSE
-
-        Redistribution and use in source and binary forms, with or without modification, are permitted provided that
-        the following conditions are met:
-
-        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
-            following disclaimer.
-        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-            the following disclaimer in the documentation and/or other materials provided with the distribution.
-        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
-            promote products derived from this software without specific prior written permission.
-
-        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-        INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-        SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-        SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-        WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-        USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-        Citation:
-            @article{diakogiannis_etal_2021,
-                title={Looking for change? Roll the dice and demand attention},
-                author={Diakogiannis, Foivos I and Waldner, Fran{\c{c}}ois and Caccetta, Peter},
-                journal={Remote Sensing},
-                volume={13},
-                number={18},
-                pages={3707},
-                year={2021},
-                publisher={MDPI}
-            }
-
-        References:
-            https://www.mdpi.com/2072-4292/13/18/3707
-            https://arxiv.org/abs/2009.02062
-            https://arxiv.org/pdf/2009.02062.pdf
-            https://github.com/waldnerf/decode/blob/9e922a2082e570e248eaee10f7a1f2f0bd852b42/FracTAL_ResUNet/nn/layers/ftnmt.py
-
-    Adapted from source code below:
-
-        MIT License
-
-        Copyright (c) 2017-2020 Matej Aleksandrov, Matej Batič, Matic Lubej, Grega Milčinski (Sinergise)
-        Copyright (c) 2017-2020 Devis Peressutti, Jernej Puc, Anže Zupanc, Lojze Žust, Jovan Višnjić (Sinergise)
-
-        Reference:
-            https://github.com/sentinel-hub/eo-flow/blob/master/eoflow/models/losses.py
-    """
-
-    def __init__(
+    def tanimoto_distance(
         self,
-        smooth: float = 1e-5,
-        weight: T.Optional[torch.Tensor] = None,
-        dim: T.Union[int, T.Sequence[int]] = 0,
-    ):
-        super().__init__()
+        y: torch.Tensor,
+        yhat: torch.Tensor,
+    ) -> torch.Tensor:
+        scale = 1.0 / self.depth
 
-        self.smooth = smooth
-        self.weight = weight
-        self.dim = dim
+        tpl = y * yhat
+        sq_sum = y**2 + yhat**2
+
+        tpl = tpl.sum(dim=self.dim, keepdims=True)
+        sq_sum = sq_sum.sum(dim=self.dim, keepdims=True)
+
+        denominator = 0.0
+        for d in range(0, self.depth):
+            a = 2.0**d
+            b = -(2.0 * a - 1.0)
+            denominator = denominator + torch.reciprocal(
+                ((a * sq_sum) + (b * tpl)) + self.smooth
+            )
+
+        numerator = tpl + self.smooth
+
+        distance = (numerator * denominator) * scale
+
+        return distance
 
     def forward(
-        self, inputs: torch.Tensor, targets: torch.Tensor
+        self,
+        inputs: torch.Tensor,
+        targets: torch.Tensor,
     ) -> torch.Tensor:
         """Performs a single forward pass.
 
         Args:
-            inputs: Predictions from model (probabilities, logits or labels).
-            targets: Ground truth values.
+            inputs: Predictions from model, shaped (B, C, H, W).
+            targets: Ground truth values, shaped (B, C, H, W).
 
         Returns:
-            Tanimoto distance loss (float)
+            Tanimoto distance
         """
 
-        def _tanimoto(yhat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-            tpl = torch.sum(yhat * y, dim=self.dim, keepdim=True)
-            sq_sum = torch.sum(yhat**2 + y**2, dim=self.dim, keepdim=True)
-            numerator = tpl + self.smooth
-            denominator = (sq_sum - tpl) + self.smooth
-            tanimoto_score = numerator / denominator
+        c1 = self.tanimoto_distance(targets, inputs)
+        c2 = self.tanimoto_distance(1.0 - targets, 1.0 - inputs)
+        c_mean = (c1 + c2) * 0.5
 
-            return tanimoto_score
-
-        score = _tanimoto(inputs, targets)
-        compl_score = _tanimoto(1.0 - inputs, 1.0 - targets)
-        score = (score + compl_score) * 0.5
-
-        return score
+        return c_mean
 
 
 class FractalAttention(nn.Module):
@@ -317,7 +230,13 @@ class FractalAttention(nn.Module):
             https://github.com/waldnerf/decode/blob/9e922a2082e570e248eaee10f7a1f2f0bd852b42/FracTAL_ResUNet/nn/layers/attention.py
     """
 
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        smooth: float = 1e-5,
+        depth: int = 5,
+    ):
         super().__init__()
 
         self.query = nn.Sequential(
@@ -351,14 +270,18 @@ class FractalAttention(nn.Module):
             nn.Sigmoid(),
         )
 
-        self.spatial_sim = TanimotoDist(dim=1)
-        self.channel_sim = TanimotoDist(dim=[2, 3])
+        self.spatial_sim = FractalTanimoto(smooth=smooth, depth=depth, dim=1)
+        self.channel_sim = FractalTanimoto(
+            smooth=smooth, depth=depth, dim=(2, 3)
+        )
         self.norm = nn.BatchNorm2d(out_channels)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        q = self.query(x)
-        k = self.key(x)
-        v = self.value(x)
+    def forward(
+        self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor
+    ) -> torch.Tensor:
+        q = self.query(q)
+        k = self.key(k)
+        v = self.value(v)
 
         attention_spatial = self.spatial_sim(q, k)
         v_spatial = attention_spatial * v
